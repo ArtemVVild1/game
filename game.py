20bb48100a2d9b01cd2398ebc32edffda960a556
ArtemVVild1 <52177826+ArtemVVild1@users.noreply.github.com>
2024-04-28 20:45:29 +0300

    Update game.py

--- game.py
+++ game.py
@@ -1,36 +1,59 @@
+import time
+
 import pygame
 
 pygame.init()
-W, H = 1200, 840
+W, H = 800, 600
 screen = pygame.display.set_mode((W, H))  # flags = pygame.NOFRAME - убрать рамку у окна
 clock = pygame.time.Clock()
 fps = 60
-action_lst = ['Idle', 'Attack', 'Dead', 'Run', 'Jump', 'Jupm_Throw', 'Jump_Attack', 'Slide', 'Throw', ]
-action = 0
-
+side = {'Right':False,'Left':False}
+action_lst = {'Idle':False, 'Attack':False, 'Dead':False, 'Run':False, 'Jump':False, 'Jupm_Throw':False, 'Jump_Attack':False, 'Slide':False, 'Throw':False, }
+action_count = {'slide_count':0,'jump_count':10}
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, ):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load(f'img/sprite/{action_lst[action]}__00{str(0)}.png')
-        self.rect = self.image.get_rect(x=500, bottom=H)
+        self.image = pygame.image.load(f'img/Idle__00{str(0)}.png')
+        self.rect = self.image.get_rect(x= W/2, bottom=H/2)
         self.jump = False
         self.jump_count = 10
-        self.slide_count = 0
-        self.right = False  # 0
-        self.left = False  # 0
         self.frame = 0
 
     def update(self, *args) -> None:
-        if self.right:
-            self.image = pygame.image.load(
-                f'img/sprite/{action_lst[action]}__00{str(int(self.frame))}.png').convert_alpha()
-            if args[2] == 3:
+        anim_key = 0
+        if action_lst['Run']:
+            if side['Right']:
                 self.rect.x += 5
-            if args[2] == 7 and self.slide_count:
-                self.slide_count -= 1
+            elif side['Left']:
+                self.rect.x -= 5
+            anim_key = 3
+
+        if action_count['slide_count'] and action_lst['Slide']:
+            if side['Right']:
                 self.rect.x += 25 / 3
-            if self.jump is True:
+            elif side['Left']:
+                self.rect.x -= 25 / 3
+            action_count['slide_count'] -= 1
+            anim_key = 7
+        if self.jump:
+            if self.jump_count >= -10:
+                if self.jump_count < 0:
+                    self.rect.y += (self.jump_count ** 2) / 2
+                else:
+                    self.rect.y -= (self.jump_count ** 2) / 2
+            self.jump_count -= 1
+        else:
+            self.jump = False
+            self.jump_count = 10
+        if side['Right']:
+            self.image = pygame.image.load(
+                    f'img/{list(action_lst)[anim_key]}__00{str(int(self.frame))}.png').convert_alpha()
+        elif side['Left']:
+            self.image = pygame.transform.flip(
+                pygame.image.load(f'img/{list(action_lst)[anim_key]}__00{str(int(self.frame))}.png'), 1,
+                0).convert_alpha()
+            """if self.jump is True:
                 if self.jump_count >= -10:
                     if self.jump_count < 0:
                         self.rect.y += (self.jump_count ** 2) // 2
@@ -39,12 +62,9 @@ class Player(pygame.sprite.Sprite):
                 self.jump_count -= 1
             else:
                 self.jump = False
-                self.jump_count = 10
-        elif self.left:
-            self.image = pygame.transform.flip(
-                pygame.image.load(f'img/sprite/{action_lst[action]}__00{str(int(self.frame))}.png'), 1,
-                0).convert_alpha()
-            if args[2] == 3:
+                self.jump_count = 10"""
+
+            """if action_lst['Run']:
                 self.rect.x -= 5
             if args[2] == 7 and self.slide_count:
                 self.slide_count += 1
@@ -58,9 +78,8 @@ class Player(pygame.sprite.Sprite):
                 self.jump_count -= 1
             else:
                 self.jump = False
-                self.jump_count = 10
-        else:
-            self.rect.bottom = H
+                self.jump_count = 10"""
+
         self.frame += 0.4
         self.frame %= 9
 
@@ -73,31 +92,31 @@ while True:
         if event.type == pygame.QUIT:
             exit()
         if event.type == pygame.KEYUP:
-            action = 0
+            action_lst = {i: False for i in action_lst}
+            action_lst['Idle'] = True
+            anim_key = 0
         if event.type == pygame.KEYDOWN:
+            action_lst = {i: False for i in action_lst}
             if event.key == pygame.K_d:
-                hero.right = True
-                hero.left = False
-                action = 3
+                action_lst['Run'] = True
+                side['Right'] = True
+                side['Left'] = False
             if event.key == pygame.K_a:
-                hero.left = True
-                hero.right = False
-                action = 3
+                action_lst['Run'] = True
+                side['Left'] = True
+                side['Right'] = False
             if event.key == pygame.K_s:
-                hero.slide_count = 30
-                action = 7
+                action_lst['Slide'] = True
+                action_count['slide_count'] = 30
             if event.key == pygame.K_w:
                 hero.jump = True
                 hero.jump_count = 10
-                action = 4
-            if event.key == pygame.K_SPACE:
-                action = 1
-            if event.key == pygame.K_q:
-                action = 8
-
+            """for i,l in action_lst.items():
+                print(i,l)
+            print('*'*100)"""
 
     screen.fill((0, 0, 0))
-    hero.update(H, W, action)
+    hero.update(H, W,)
     screen.blit(hero.image, hero.rect)
     pygame.display.update()
-    clock.tick(fps)
+    clock.tick(fps)s)
